0b4a64639927e78dab90aa00abceeb23b1ea6297
Johan Fredin <johan.fredin@consid.se>
2019-07-23 13:49:35 +0200

    CRUD rest api with mongo db in place

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,71 +1,89 @@
 const express = require('express')
-const members = require('../../Members')
 const uuid = require('uuid')
+const mongoose = require('mongoose')
 const router = express.Router()
+const dbUrl = 'mongodb://localhost:27017/members'
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log(`Could not connect to database at ${dbUrl}`)
+    } else {
+        console.log('Connection successful')
+    }
+})
+const MemberModel = mongoose.model('Member', {
+    name: String,
+    email: String,
+    active: String
+})
 
 // gets all members 
-router.get('/', (req, res) => res.json(members))
+router.get('/', (req, res) => {
+    MemberModel.find().lean().exec((err, members) => {
+        if (err) {
+            return res.json({ msg: 'Could not retrieve members' })
+        }
+        res.json(members)
+    })
+})
 
 // Get a single member
 router.get('/:id', (req, res) => {
-    const requestedId = parseInt(req.params.id)
-    const found = members.some(member => member.id === requestedId)
-    if (found) {
-        res.json(members.filter(member => member.id === requestedId))
-    } else {
-        res.status(400).json({ msg: `No member with id ${requestedId}` })
-    }
+    MemberModel.findById(req.params.id).lean().exec((err, member) => {
+        if (err) {
+            res.status(400).json({ msg: `No member with id ${req.params.id}` })
+        } else {
+            res.json(member)
+        }
+    })
 })
 
 // Create member
 router.post('/', (req, res) => {
-    const newMember = {
-        id: uuid.v4(), // generate new uuid
+    const newMember = new MemberModel({
         name: req.body.name,
         email: req.body.email,
         status: "active"
-    }
+    })
 
     if (!newMember.name || !newMember.email) {
         return res.status(400).json({ msg: 'Please include a name and email' })
     }
 
-    members.push(newMember)
-    res.json(members) // return all members
+    newMember.save().then(() => console.log('Member persisted'))
+    res.json(newMember) // return all members
 })
 
 // Update member
 router.put('/:id', (req, res) => {
-    const requestedId = parseInt(req.params.id)
-    const found = members.some(member => member.id === requestedId)
-    if (found) {
-        const updateMember = req.body
-        members.forEach(member => {
-            if(member.id === parseInt(req.params.id)) {
-                member.name = updateMember.name ? updateMember.name : member.name
-                member.email = updateMember.email ? updateMember.email : member.email
-
-                res.json({msg: 'Member was updated', member})
-            }
-        })
-        res.json(members.filter(member => member.id === requestedId))
-    } else {
-        res.status(400).json({ msg: `No member with id ${requestedId}` })
+    
+    const updateMember = {
+        name: req.body.name,
+        email: req.body.email
     }
+
+    console.log(updateMember)
+
+    MemberModel.findById(req.params.id, (err, member) => {
+        if (err) {
+            res.status(400).json({ msg: `No member with id ${req.params.id}` })
+        } else {
+            member.name = updateMember.name
+            member.email = updateMember.email
+            member.save().then(() => res.json({msg: 'Member updated', member}))
+        }
+    })
 })
 
 // Delete member
 router.delete('/:id', (req, res) => {
-    const requestedId = parseInt(req.params.id)
-    const found = members.some(member => member.id === requestedId)
-    if (found) {
-        res.json({
-            msg: 'Member deleted',
-            members: members.filter(member => member.id !== requestedId)
-        })
-    } else {
-        res.status(400).json({ msg: `No member with id ${requestedId}` })
-    }
+    MemberModel.findByIdAndDelete(req.params.id, (err, member) => {
+        if (err) {
+            res.json({ msg: 'Could not remove member with id ' + req.params.id })
+        } else {
+            console.log('Member deleted')
+        }
+    })
 })
 
 module.exports = router
